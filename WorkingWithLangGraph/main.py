4ab1d1b511b7670d0b47d19a70a6a27d64bb19d6
Abdullah Shahzad <114865890+hafiz-Abdullahshahzad-DS@users.noreply.github.com>
2024-09-14 08:11:50 +0500

    Real Time Question Answering via LangGraph

--- WorkingWithLangGraph/main.py
+++ WorkingWithLangGraph/main.py
@@ -1,66 +1,155 @@
-from dotenv import load_dotenv
-import os
-from typing import Annotated
-from typing_extensions import TypedDict
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langchain_google_genai import ChatGoogleGenerativeAI
-#import the API keys
+from IPython.display import Image, display
+# from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.messages import ToolMessage
+from dotenv import load_dotenv
+from typing import Annotated, List,Literal
+from typing_extensions import TypedDict
+from langchain_core.messages import BaseMessage,AIMessage
 
+import os
+import json 
 load_dotenv()
+os.environ["TAVILY_API_KEY"] = os.environ.get("TAVILY_API_KEY")
+google_api_key = os.environ.get("GOOGLE_API_KEY")
 
-anthropic_api_key = os.environ.get("anthropic_api_key")
-os.environ["GOOGLE_API_KEY"] = os.environ.get("GOOGLE_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "LangGraph Tutorial"
+llm = ChatGoogleGenerativeAI(model="gemini-1.0-pro-latest", google_api_key=google_api_key)
 
+# print(llm.invoke("Hello, how are you?"))
 
-# The first thing you do when you define a graph is define the State of the graph. 
-# The State consists of the schema of the graph as well as reducer functions 
-# which specify how to apply updates to the state. In our example State is a TypedDict with a single key: messages.
-#  The messages key is annotated with the add_messages reducer function, 
-# which tells LangGraph to append new messages to the existing list, rather than overwriting it. 
-# State keys without an annotation will be overwritten by each update, storing the most recent value. 
-# Check out this conceptual guide to learn more about state, reducers and other low-level concepts.
 
 class State(TypedDict):
-    # consist of Schema of graph as well as reducer functions
+    messages : Annotated[List,add_messages]
 
-    messages : Annotated[list, add_messages]
+
 
 graph_builder = StateGraph(State)
 
-
-from langchain_groq import ChatGroq
-
-# llm = ChatGroq(api_key=GROQ_API_KEY, model="mixtral-8x7b-32768")
-llm = ChatGoogleGenerativeAI(model="gemini-1.0-pro-latest")
-
-# print(llm.invoke("Hello, how are you?"))
-def chatbot(state: State):
+def chatbot(state:State):
     return {"messages": [llm.invoke(state["messages"])]}
 
-
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
 graph_builder.add_node("chatbot", chatbot)
+
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
+
 graph = graph_builder.compile()
 
-# The graph is now ready to be used.
+from IPython.display import Image, display
 
-# from IPython.display import Image, display
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
 
-# try:
-#     display(Image(graph.get_graph().draw_mermaid_png()))
-#     # save the image as well
-#     graph.get_graph().draw_mermaid_png().save("graph.png")
-# except Exception:
-#     # This requires some extra dependencies and is optional
-#     pass
+
+# while True:
+#     user_input = input("User: ")
+#     if user_input.lower() in ["quit","q","exit"]:
+#         print("Goodbye Bro I am going!")
+#         break
+#     for event in graph.stream({"messages": ("user", user_input)}):
+#         for value in event.values():
+#             print("Assistant:", value["messages"][-1].content)
+
+travily_tool = TavilySearchResults(max_results=5)
+print(travily_tool.description)
+tools = [travily_tool]
+
+# search_query = "Tell me whether in Faisalabad,pakistan right now.is it raining? "
+# for response in travily_tool.invoke(search_query):
+#     print(response["content"], end="\n\n" + "*" * 50 + "\n\n")
+
+llm_with_tools = llm.bind_tools(tools)
+def ToolEnabledChatbot(state:State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+# tool_node = ToolNode(tools)
+# graph_builder.add_node("tool_node", tool_node)
+
+class BasicToolNode:
+
+    def __init__(self, tools:list)->None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+    def __call__(self,inputs:dict):
+        if messages := inputs.get("messages",[]):
+            message = messages[-1]
+        else:
+            raise ValueError("No messages found in inputs")
+        output = []
+        for tool_call in message.tool_calls:
+            print("Calling  Travily Tool")
+            tool_result = self.tools_by_name["tavily_search_results_json"].invoke(
+                tool_call["args"]
+            )
+            # print(tool_call)
+            output.append(
+                ToolMessage(
+                    content = json.dumps(tool_result),
+                    name = tool_call["name"],
+                    tool_call_id = tool_call["id"]
+                )
+            )
+
+        return {"messages": output}
+    
+
+graph_builder_with_tools = StateGraph(State)
+
+graph_builder_with_tools.add_node("ToolEnabledChatbot", ToolEnabledChatbot)
+
+# create a tool node
+
+graph_builder_with_tools.add_node("tools", BasicToolNode(tools = [travily_tool]))
+
+# BOTH NODES ARE DEFINED NOW WE NEED TO DEFINE THE CONDITIONal edges
+
+def route_tools(state: State) -> Literal["tools", "__end__"]:
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return "__end__"
+
+graph_builder_with_tools.add_conditional_edges(
+    "ToolEnabledChatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", "__end__": "__end__"},
+)
+
+
+graph_builder_with_tools.add_edge("tools", "ToolEnabledChatbot")
+graph_builder_with_tools.add_edge(START, "ToolEnabledChatbot")
+Toolgraph = graph_builder_with_tools.compile()
+
+
+try:
+    display(Image(Toolgraph.get_graph().draw_mermaid_png()))
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+
+
+
+
 
 
 while True:
@@ -68,7 +157,7 @@ while True:
     if user_input.lower() in ["quit", "exit", "q"]:
         print("Goodbye!")
         break
-    for event in graph.stream({"messages": ("user", user_input)}):
+    for event in Toolgraph.stream({"messages": [("user", user_input)]}):
         for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
+            if isinstance(value["messages"][-1], AIMessage):
+                print("Assistant:", value["messages"][-1].content)
